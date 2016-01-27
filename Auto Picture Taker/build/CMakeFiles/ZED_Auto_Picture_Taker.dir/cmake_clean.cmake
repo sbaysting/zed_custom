6492c3535d581bca07fe64be14ded509d53c1f66
Samuel Baysting <samuelbaysting@gmail.com>
2016-01-26 20:50:54 -0500

    Initial commit is a copy of the ZED sample 'Save depth'

---
+++ Auto Picture Taker/build/CMakeFiles/ZED_Auto_Picture_Taker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ZED_Auto_Picture_Taker.dir/src/main.o"
+  "ZED Auto Picture Taker.pdb"
+  "ZED Auto Picture Taker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ZED_Auto_Picture_Taker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
